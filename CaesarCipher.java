e6ab710de7f9b8c81be9ec827869843c981e887a
cHurm16 <chloe.hurm@stu.daviess.kyschools.us>
2025-02-24 13:54:33 +0000

    homework

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -9,12 +9,13 @@
 
 public class CaesarCipher {
 
-	private char[] alphabet;
+	private char[] alphabet ('a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+    'o','p','q','r','s','t','u','v','w','x','y','z');
 	private char[] shifted;
 	private int shift;
 
     public CaesarCipher() {
-
+        int i=0;
     }
 
     public CaesarCipher(int num){
